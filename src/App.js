ff729d3921dc3673b6df664413350937462a2fec
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-10-09 15:55:46 +1100

    new style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react';
 
+import './App.css'
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+// import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './store/auth-context';
+import illuPerso from './assets/illu.png';
 
 function App() {
   const ctx = useContext(AuthContext);
 
   return (
     <React.Fragment>
-      <MainHeader />
-      <main>
+      {/* <MainHeader /> */}
+      <main className='login'>
+        <img src={illuPerso} alt='A man next to a tree' />
         {!ctx.isLoggedIn && <Login />}
         {ctx.isLoggedIn && <Home />}
       </main>
